12e20e23727534617ca6312f887cb92e82f6049c
Andrew Forward <aforward@gmail.com>
2017-07-20 11:51:05 -0400

    Calculate current version from the file, as the config is cached

--- lib/mix/tasks/current.ex
+++ lib/mix/tasks/current.ex
@@ -1,5 +1,6 @@
 defmodule Mix.Tasks.Version.Current do
   use Mix.Task
+  use FnExpr
 
   @shortdoc "The current version of your project"
   def run(args) do
@@ -9,6 +10,28 @@ defmodule Mix.Tasks.Version.Current do
   end
 
   @shortdoc "Calculate the current version"
-  def calc(_), do: Mix.Project.config[:version]
+  def calc(_) do
+    "mix.exs"
+    |> File.read
+    |> invoke(fn ({:ok, content}) -> content end)
+    |> String.split("\n")
+    |> Enum.map(&find_version/1)
+    |> Enum.reject(&is_nil/1)
+    |> first
+  end
+
+  defp find_version(line) do
+    case Regex.run(~r{@version \"(.*)\"}, line) do
+      nil -> nil
+      [_, version] -> version
+    end
+  end
+
+  defp first(list) do
+    case Enum.fetch(list, 0) do
+      {:ok, val} -> val
+      :error -> nil
+    end
+  end
 
 end
