280139991c6c6270afac75986e9381a5bfed649a
Andrew Forward <aforward@gmail.com>
2017-08-27 07:30:19 -0400

    Add a 'mix version.name' task to determine if this is a major, minor and patch uptick

---
+++ lib/mix/tasks/name.ex
@@ -0,0 +1,22 @@
+defmodule Mix.Tasks.Version.Name do
+  use Mix.Task
+  use VersionTasks.FnExpr
+  alias Mix.Tasks.Version.Current
+
+  @shortdoc "Return the name (major|minor|patch) of this version"
+  def run(_) do
+    calc() |> IO.puts
+  end
+
+  def calc(), do: calc(Current.calc)
+  def calc(version) do
+    version
+    |> String.split(".")
+    |> name
+  end
+
+  defp name([_, "0", _]), do: :major
+  defp name([_, _, "0"]), do: :minor
+  defp name(_), do: :patch
+
+end
---
+++ test/tasks/name_test.exs
@@ -0,0 +1,15 @@
+defmodule Mix.Tasks.Version.NameTest do
+  use ExUnit.Case
+  alias Mix.Tasks.Version.Name
+
+  test "name" do
+    assert :patch == Name.calc("1.2.3")
+    assert :patch == Name.calc("1.2.30")
+
+    assert :minor == Name.calc("0.1.0")
+    assert :minor == Name.calc("1.4.0")
+
+    assert :major == Name.calc("1.0.0")
+    assert :major == Name.calc("2.0.0")
+  end
+end
